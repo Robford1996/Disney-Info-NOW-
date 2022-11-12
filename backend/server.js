2ab804e892f0f41f6f06a5f0238e95f2cd976171
Robert Ford <robert.ford1996@gmail.com>
2022-11-11 23:02:38 -0500

    connect mongoose

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,28 @@
 // get .env variables
 require("dotenv").config();
 //pull PORT from .env, give default value 4000
-const { PORT = 4000 } = process.env;
+const { PORT = 4000, MONGODB_URL } = process.env;
 // import express
 const express = require("express");
 //create app object
 const app = express();
+//import mongoose
+const mongoose = require("mongoose");
+//import middleware
+const cors = require("cors");
+const morgan = require("morgan");
+
+////////////////
+//DATABASE CONNECTION
+/////////////////
+//Establish connection
+mongoose.connect(MONGODB_URL);
+
+//Connection Events
+mongoose.connection
+  .on("open", () => console.log("You are connected to Mongoose"))
+  .on("close", () => console.log("You are disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
 
 /////////////////////
 //ROUTES
